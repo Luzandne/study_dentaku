0db4324328543659eb7080a9e49d55dcd2825306
luzandne <luzandne@gmail.com>
2018-01-21 18:23:23 +0900

    電卓もどき完成

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -10,10 +10,7 @@
 
 @interface ViewController : UIViewController
 
-// ラベルボタン
-@property UILabel *labelButton;
-
-
+// 0~9の数字ラベルボタン
 @property UILabel *label0;
 
 @property UILabel *label1;
@@ -33,27 +30,38 @@
 @property UILabel *label8;
 
 @property UILabel *label9;
-
-// 結果出力ラベル
-@property UILabel *labelResult;
+// ＝ラベル
+@property UILabel *labelEqual;
+// ＋ラベル
+@property UILabel *labelAddition;
+// ーラベル
+@property UILabel *labelSubtraction;
+// ×ラベル
+@property UILabel *labelMultiplication;
+// ÷ラベル
+@property UILabel *labelDivision;
+// ACラベル
+@property UILabel *labelAc;
 // 計算モードラベル
 @property UILabel *labelCalMode;
+// 結果出力ラベル
+@property UILabel *labelResult;
 // 入力値
 @property int inputValue;
-// 数値退避
-@property int evadeInputValue;
-// 結果
-@property int result;
-// 退避
-@property int evacuation;
-// trueなら計算中　falseなら数値入力中
+// 数値退避１回目計算時
+@property int evacuationInputValue1st;
+// 数値退避２回目計算時
+@property int evacuationInputValue2nd;
+// 結果を出すタイミング
+@property BOOL isResultTime;
+// 結果を出すタイミング
+@property BOOL reInput;
+// trueなら２回目　falseなら１回目の数値入力中
 @property BOOL isCalculation;
-// モード
-@property NSString *inputMode;
 // 初回入力フラグ
 @property BOOL inputFirst;
 
-// モードを用意したけどenumの呼び方が分からない・・・
+// モードを用意したけどenumの呼び方が分からない・・・情けなし
 //@property enum calMode{
 //    addition,
 //    subtraction,
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.inputFirst = true;
     CGRect windowRange = [[UIScreen mainScreen] bounds];
     int centerWidth = windowRange.size.width / 2;
     int centerHeight = windowRange.size.height / 2;
@@ -52,24 +51,11 @@
     // 結果ラベルのY座標
     int lineResultPosition = centerHeight - spaceHeight / 2;
     // 入力値初期化
-    self.inputValue = 0;
+    [self allClear];
     
-//    int counter = 0;
-//    // 数字ボタン(1~9まで)配置
-//    for (int i = 1; i <= 3 ;i++){       //列
-//        for(int j = 1; j <= 3; j++){    //行
-//            ++counter;
-//            self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition + (spaceWidth * 6 * (j-1)), line2Position - (spaceHeight * 3 * (i - 1)), labelButtonWidth, labelButtonHeight)];
-//            self.labelButton.text = [NSString stringWithFormat:@"%d", counter];;
-//            self.labelButton.tag = counter;
-//            [self labelButtonSetting:self.labelButton];
-//            [self.view addSubview:self.labelButton];
-//        }
-//
-//    }
     self.label0 = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, line1Position, labelButtonWidth, labelButtonHeight)];
     self.label0.text = @"0";
-    self.label0.tag = 0;
+    self.label0.tag = -1;    //0はタグ未設定値で不具合の元だから別の値にする
     [self labelButtonSetting:self.label0];
     [self.view addSubview:self.label0];
     
@@ -99,7 +85,7 @@
     
     self.label5 = [[UILabel alloc]initWithFrame:CGRectMake(lineCenterPosition, line3Position, labelButtonWidth, labelButtonHeight)];
     self.label5.text = @"5";
-    self.label5.tag = 6;
+    self.label5.tag = 5;
     [self labelButtonSetting:self.label5];
     [self.view addSubview:self.label5];
     
@@ -126,59 +112,47 @@
     self.label9.tag = 9;
     [self labelButtonSetting:self.label9];
     [self.view addSubview:self.label9];
-    // 数字ボタン0配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, line1Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"0";
-    self.labelButton.tag = 0;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
 
-//    // 小数点ボタン配置
-//    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineCenterPosition, line1Position, labelButtonWidth, labelButtonHeight)];
-//    self.labelButton.text = @".";
-//    self.labelButton.tag = 10;
-//    [self labelButtonSetting:self.labelButton];
-//    [self.view addSubview:self.labelButton];
     // =ボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line1Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"=";
-    self.labelButton.tag = 11;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
+    self.labelEqual = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line1Position, labelButtonWidth, labelButtonHeight)];
+    self.labelEqual.text = @"=";
+    self.labelEqual.tag = 11;
+    [self labelButtonSetting:self.labelEqual];
+    [self.view addSubview:self.labelEqual];
     // +ボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line5Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"+";
-    self.labelButton.tag = 12;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
+    self.labelAddition = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line5Position, labelButtonWidth, labelButtonHeight)];
+    self.labelAddition.text = @"+";
+    self.labelAddition.tag = 12;
+    [self labelButtonSetting:self.labelAddition];
+    [self.view addSubview:self.labelAddition];
     // -ボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line6Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"-";
-    self.labelButton.tag = 13;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
+    self.labelSubtraction = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line6Position, labelButtonWidth, labelButtonHeight)];
+    self.labelSubtraction.text = @"-";
+    self.labelSubtraction.tag = 13;
+    [self labelButtonSetting:self.labelSubtraction];
+    [self.view addSubview:self.labelSubtraction];
     // ×ボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line7Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"×";
-    self.labelButton.tag = 14;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
+    self.labelMultiplication = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line7Position, labelButtonWidth, labelButtonHeight)];
+    self.labelMultiplication.text = @"×";
+    self.labelMultiplication.tag = 14;
+    [self labelButtonSetting:self.labelMultiplication];
+    [self.view addSubview:self.labelMultiplication];
     // ÷ボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line8Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"÷";
-    self.labelButton.tag = 15;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
+    self.labelDivision = [[UILabel alloc]initWithFrame:CGRectMake(lineRightPosition, line8Position, labelButtonWidth, labelButtonHeight)];
+    self.labelDivision.text = @"÷";
+    self.labelDivision.tag = 15;
+    [self labelButtonSetting:self.labelDivision];
+    [self.view addSubview:self.labelDivision];
     // オールクリアボタン配置
-    self.labelButton = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, line8Position, labelButtonWidth, labelButtonHeight)];
-    self.labelButton.text = @"全部取消";
-    self.labelButton.tag = 16;
-    [self labelButtonSetting:self.labelButton];
-    [self.view addSubview:self.labelButton];
-    // モードボタン配置
+    self.labelAc = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, line8Position, labelButtonWidth, labelButtonHeight)];
+    self.labelAc.text = @"全部取消";
+    self.labelAc.tag = 16;
+    [self labelButtonSetting:self.labelAc];
+    [self.view addSubview:self.labelAc];
+    // モードラベル配置
     self.labelCalMode = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, line7Position, labelButtonWidth, labelButtonHeight)];
     self.labelCalMode.text = @"";
-    [self labelButtonSetting:self.labelCalMode];
+    [self labelModeSetting:self.labelCalMode];
     [self.view addSubview:self.labelCalMode];
     // 計算結果ラベル配置
     self.labelResult = [[UILabel alloc]initWithFrame:CGRectMake(lineLeftPosition, lineResultPosition, labelResultWidth, labelResultHeight)];
@@ -202,6 +176,20 @@
     return labelButton;
 }
 
+// モードラベルボタン設定
+-(UILabel *)labelModeSetting:(UILabel *)labelMode {
+    labelMode.backgroundColor = [UIColor whiteColor];
+    labelMode.textColor = [UIColor blackColor];
+    labelMode.textAlignment = NSTextAlignmentCenter;
+    labelMode.userInteractionEnabled = YES;
+    //枠線
+    labelMode.layer.borderWidth = 1.0f;
+    //枠線の色
+    labelMode.layer.borderColor = [[UIColor blackColor] CGColor];
+    
+    return labelMode;
+}
+
 // 結果ラベル設定
 -(UILabel *)labelResultSetting:(UILabel *)labelResult {
     labelResult.backgroundColor = [UIColor whiteColor];
@@ -221,99 +209,153 @@
     UITouch *touch = [[event allTouches] anyObject];
     //タッチイベント
     switch (touch.view.tag) {
-        case 0:
+        case -1:
             self.inputValue = 0;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 1:
             self.inputValue = 1;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 2:
             self.inputValue = 2;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 3:
             self.inputValue = 3;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 4:
             self.inputValue = 4;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 5:
             self.inputValue = 5;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 6:
             self.inputValue = 6;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 7:
             self.inputValue = 7;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 8:
             self.inputValue = 8;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
         case 9:
             self.inputValue = 9;
-            [self reloadStatus];
+            [self callReloadInput];
             break;
 //        case 10:
 //            break;
         case 11:    // =
-            self.inputMode = @"equal";
-            self.isCalculation = false;
-            self.inputFirst = true;
+            [self outputCalResult];
             break;
         case 12:    // 足し算
-            self.inputMode = @"addition";
             self.isCalculation = true;
+            self.labelCalMode.text = @"＋";
+            self.isResultTime = false;
+            self.reInput = false;
             break;
         case 13:    // 引き算
-            self.inputMode = @"subtraction";
             self.isCalculation = true;
-            [self reloadStatus];
+            self.labelCalMode.text = @"ー";
+            self.isResultTime = false;
+            self.reInput = false;
             break;
         case 14:    // 掛け算
-            self.inputMode = @"multiplication";
             self.isCalculation = true;
-            [self reloadStatus];
+            self.labelCalMode.text = @"×";
+            self.isResultTime = false;
+            self.reInput = false;
             break;
         case 15:    // 割り算
-            self.inputMode = @"division";
             self.isCalculation = true;
-            [self reloadStatus];
+            self.labelCalMode.text = @"÷";
+            self.isResultTime = false;
+            self.reInput = false;
             break;
         case 16:    // オールクリア
-            self.inputMode = @"AC";
-            [self reloadStatus];
+            [self allClear];
             break;
         default:
             break;
     }
 }
 
--(void)reloadStatus {
-    if(self.isCalculation){
-        
+-(void)outputCalResult {
+    if([self.labelCalMode.text isEqualToString:@"＋"]) {
+        self.evacuationInputValue1st += self.evacuationInputValue2nd;
+    } else if([self.labelCalMode.text isEqualToString:@"ー"]) {
+        self.evacuationInputValue1st -= self.evacuationInputValue2nd;
+    } else if([self.labelCalMode.text isEqualToString:@"×"]) {
+        self.evacuationInputValue1st *= self.evacuationInputValue2nd;
+    } else if([self.labelCalMode.text isEqualToString:@"÷"]) {
+        self.evacuationInputValue1st /= self.evacuationInputValue2nd;
     } else {
-        if(self.evadeInputValue != 0){
-            NSString *str = [NSString stringWithFormat:@"%d%d",self.evadeInputValue,self.inputValue];
-            self.labelResult.text = str;
-        } else {
-            self.labelResult.text = [NSString stringWithFormat:@"%d",self.inputValue];
-        }
-        self.evadeInputValue = [self.labelResult.text intValue];
-        if(self.evadeInputValue >= 2147483647) {
-            self.labelResult.text = [NSString stringWithFormat:@"%d",self.evadeInputValue];
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:@"桁あふれです" preferredStyle:UIAlertControllerStyleAlert];
-            [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-            [self presentViewController:alert animated:YES completion:nil];
-        }
+        // な　に　も　し　な　い
     }
+    self.evacuationInputValue2nd = 0;
+    self.labelCalMode.text = @"";
+    self.isCalculation = false;
+    self.isResultTime = true;
+    self.inputValue = self.evacuationInputValue1st;
+    [self callReloadInput];
+}
+
+-(void)allClear {
+    self.labelResult.text = @"";
+    [self waitMs];
+    self.inputValue = 0;
+    self.evacuationInputValue1st = 0;
+    self.evacuationInputValue2nd = 0;
+    self.labelCalMode.text = @"";
+    self.labelResult.text = @"0";
+    self.isCalculation = false;
+    self.isResultTime = false;
+    self.reInput = false;
+}
+
+// TODO 電卓でよくある点滅のやつ・・・だけど実装がうまくいかない
+-(void)waitMs {
+    self.labelResult.text = @"";
+}
+
+-(void)callReloadInput {
+    if(self.isCalculation){
+        self.evacuationInputValue2nd = [self inputValueReload:self.evacuationInputValue2nd];
+    } else {
+        self.evacuationInputValue1st = [self inputValueReload:self.evacuationInputValue1st];
+    }
+}
+
+// 入出力を画面に反映させる細かい作業はここで行う
+// 計算結果の出力と入力値の更新は別々にさせておけばよかった。。
+-(int)inputValueReload:(int)evacuationInputValue {
+    [self waitMs];
+    if((evacuationInputValue != 0 && !self.isResultTime) && !self.reInput){
+        NSString *str = [NSString stringWithFormat:@"%d%d",evacuationInputValue,self.inputValue];
+        self.labelResult.text = str;
+    } else if(evacuationInputValue != 0 && self.isResultTime) {
+        self.labelResult.text = [NSString stringWithFormat:@"%d",self.inputValue];
+        self.isResultTime = false;
+        self.reInput = true;
+    } else { // 計算結果を出して再度入力しようとすると値をリセットする。
+        self.labelResult.text = [NSString stringWithFormat:@"%d",self.inputValue];
+        self.isResultTime = false;
+        self.reInput = false;
+    }
+    evacuationInputValue = [self.labelResult.text intValue];
+    if(evacuationInputValue >= 2147483647) {
+        self.labelResult.text = [NSString stringWithFormat:@"%d",evacuationInputValue];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:@"これより大きな値は計算できませんorz" preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    return evacuationInputValue;
 }
 
 - (void)didReceiveMemoryWarning {
